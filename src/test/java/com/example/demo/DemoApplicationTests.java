99d5236927af9b7b6e8f190c84b83b13e3053734
jlinder <john.linder@sogeti.se>
2023-03-13 10:43:20 +0100

    Initial commit

---
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -0,0 +1,52 @@
+package com.example.demo;
+
+import io.restassured.RestAssured;
+import io.restassured.http.Method;
+import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import static io.restassured.RestAssured.given;
+@SpringBootTest
+class DemoApplicationTests {
+
+	String baseurl= "http://localhost:8080";
+
+
+
+	@Test
+	void getgreeting() throws JSONException {
+		RestAssured.baseURI = baseurl;
+		// Get the RequestSpecification of the request to be sent to the server.
+		RequestSpecification httpRequest = RestAssured.given();
+		// specify the method type (GET) and the parameters if any.
+		//In this case the request does not take any parameters
+		httpRequest.param("name", "Kavya");
+		Response response = httpRequest.get("/greeting");
+
+		// Print the status and message body of the response received from the server
+		System.out.println("Status received => " + response.getStatusLine());
+
+
+	}
+
+	@Test
+	void postgreeting() throws JSONException {
+		RestAssured.baseURI = baseurl;
+		// Get the RequestSpecification of the request to be sent to the server.
+		RequestSpecification httpRequest = RestAssured.given();
+		// specify the method type (GET) and the parameters if any.
+		//In this case the request does not take any parameters
+		httpRequest.param("name", "Kavya");
+		Response response = httpRequest.get("/greeting");
+
+		// Print the status and message body of the response received from the server
+		System.out.println("Status received => " + response.getStatusLine());
+
+
+	}
+
+
+}
